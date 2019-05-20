5ee2313d915ded133e1212e1128b57f05c07e655
Akhilesh12859 <akhilesh.sinhs2018@gmail.com>
2019-05-20 14:16:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/edutech/minevideotube/Config.java
@@ -0,0 +1,13 @@
+package com.example.edutech.minevideotube;
+
+
+public final class Config {
+
+    private Config() {
+
+
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDUUIWJeEAVf1lwjA8MGjB91fkCfH7WLGs";
+
+}
